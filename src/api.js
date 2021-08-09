6a286893854f058d46003b838ea9f4cda7d6e1d8
anamad8 <stellam_anabel@hotmail.com>
2021-08-09 13:26:06 -0300

    clase6

--- src/api.js
+++ src/api.js
@@ -1,42 +1,39 @@
-const api = {
-    status: "success",
-    items: [
+const api = [
     {
         id: 1,
-        title: "Buzo Bulgaro",
+        titel: "Buzo Bulgaro",
         price: 665,
-        image: require('./components/img/ropaMujer/Buzo-Bulgaro.jpg'),
+        imagen: require('./components/img/ropaMujer/Buzo-Bulgaro.jpg'),
         cantidad: 1,
     },
     {
         id: 2,
-        title: "Buzo rustico con botones en un hombro",
+        titel: "Buzo rustico con botones en un hombro",
         price: 745,
-        image: require('./components/img/ropaMujer/Buzo-rustico-con-botones-en-un-hombro.png'),
+        imagen: require('./components/img/ropaMujer/Buzo-rustico-con-botones-en-un-hombro.png'),
         cantidad: 1
     },
     {
         id: 3,
-        title: "Camisa de Viyela",
+        titel: "Camisa de Viyela",
         price: 475,
-        image: require('./components/img/ropaMujer/Camisa-de-Viyela.jpg'),
+        imagen: require('./components/img/ropaMujer/Camisa-de-Viyela.jpg'),
         cantidad: 1
     },
     {
         id: 4,
-        title: "Camisa sin frunces manga larga",
+        titel: "Camisa sin frunces manga larga",
         price: 399,
-        image: require('./components/img/ropaMujer/CAMISA-SIN-FRUNCES-MANGA-LARGA.png'),
+        imagen: require('./components/img/ropaMujer/CAMISA-SIN-FRUNCES-MANGA-LARGA.png'),
         cantidad: 1
     },
     {
         id: 5,
-        title: "Chaleco amplio de bremer cuello V",
+        titel: "Chaleco amplio de bremer cuello V",
         price: 405,
-        image: require('./components/img/ropaMujer/Chaleco-amplio-de-bremer-cuello-V.jpg'),
+        imagen: require('./components/img/ropaMujer/Chaleco-amplio-de-bremer-cuello-V.jpg'),
         cantidad: 1
     }
-    ],
-};
+];
 
     export default api;
--- src/components/ItemListContainer/ItemLisContainer.js
+++ src/components/ItemListContainer/ItemLisContainer.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Card, Button} from 'react-bootstrap';
 import './ItemLisContainer.css';
 import imgMujer from '../img/mujer.jpg'
 import imgHombre from '../img/hombre.jpg'
+import Api from '../../api'
+
+
+
+const promesa = new Promise((resolve, reject) => {
+    setTimeout(function(){
+        resolve(Api); 
+    }, 1000);
+});
+
+    promesa.then(console.log);
+
+    const productos = onSeleccionarProductos => {
+        return productos.map(productos =>{
+            const{id, titel, imagen } = productos;
+            return(
+                <Api
+                    key={id}
+                    titel={titel}
+                    imagen={imagen}
+                />
+            )
+        })
+    }
 
 function ItemLisContainer() {
     return (
@@ -11,6 +35,19 @@ function ItemLisContainer() {
                 <Col className="container-fluid p-2 bd-higlight mujerHombre"><a href="#"><img img className="mujerHombre" src={ imgMujer } /></a> </Col>
                 <Col className="container-fluid p-2 bd-higlight mujerHombre"><a href="#"><img className="mujerHombre" src={ imgHombre } /></a> </Col>
             </Row>
+            <div className="productos">
+                <Card className="producto">
+                    <Card.Img variant="top" className="productoImg" src={Api.imagen} />
+                    <Card.Body>
+                        <Card.Title className="titulo">{Api.titel}</Card.Title>
+                        <Card.Text>
+                            <p className="precio">${Api.precio}</p>
+                        </Card.Text>
+                        <Button variant="primary">Agregar al Carrito</Button>
+                    </Card.Body>
+                </Card>
+                
+            </div>
         </Container>
     );
 }
